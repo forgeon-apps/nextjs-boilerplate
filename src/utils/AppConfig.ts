59591026d21eb9657dc01d93a2170d4bea3be786
forgeon-dev <forgeon.io@gmail.com>
2025-08-29 13:38:22 +0700

    update AppConfig.ts - title back to V4

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
   site_name: 'Starter',
-  title: 'Next.js Boilerplate (v7)',
+  title: 'Next.js Boilerplate (v4)',
   description:
     'Starter code for your Next.js blog Boilerplate with Tailwind CSS',
   url: 'https://example.com',
