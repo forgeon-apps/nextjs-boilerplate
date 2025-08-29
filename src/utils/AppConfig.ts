6502cfc03ecd53f7fdfc0b7c7588cdee8b86ecdc
Iam ByeBlog`s <iam.byeblogs@gmail.com>
2025-08-29 11:43:54 +0700

    Update AppConfig.ts
    update title to v3

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
   site_name: 'Starter',
-  title: 'Next.js Boilerplate (v4)',
+  title: 'Next.js Boilerplate (v3)',
   description:
     'Starter code for your Next.js blog Boilerplate with Tailwind CSS',
   url: 'https://example.com',
